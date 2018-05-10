c07a6dd46647e9bdaf3b37946cb342da48130a4b
wangli0518 <www.617844338@qq.com>
2018-05-10 11:12:43 +0800

    新

--- my-react-app/src/page/Home/Home.js
+++ my-react-app/src/page/Home/Home.js
@@ -1,29 +1,36 @@
 
 import ReactDom from 'react-dom';
 import React, { Component } from 'react';
-import {render} from 'react-dom'
-import Header from './Header'
-<<<<<<< HEAD
-import  Slider from './Slider'
-import HomeLittleList from './HomeLittleList'
-import  RunLeft from './RunLeft'
-import  RunRight from './RunRight'
-import BotList from './BotList'
-import GoTop from './GoTop'
-
-
+import {render} from 'react-dom';
+import Header from './Header';
+import  Slider from './Slider';
+import HomeLittleList from './HomeLittleList';
+import  RunLeft from './RunLeft';
+import  RunRight from './RunRight';
+import BotList from './BotList';
+import GoTop from './GoTop';
+import './Home.less';
+import axios from '../../api';
+import {getSlider} from "../../api/home";
 export default class Home extends Component {
-
-=======
-import axios from '../../api'
-import Slider from './Slider'
-export default class Home extends Component {
->>>>>>> 5fdde48b3130c0baa58526f444389fb96b2acf80
+    constructor(){
+        super();
+        this.state={slider:[]}
+    }
+    getData=()=>{
+        getSlider().then((data) => {
+            this.setState({slider:data});
+        });
+    };
+    componentDidMount() {
+        this.getData();
+    }
     render(){
         return(
             <div>
                 <Header/>
-                <Slider/>
+                {this.state.slider.length > 0 ?
+                    <Slider lists={this.state.slider} /> : "加载中"}
                 <HomeLittleList/>
                 <RunLeft/>
                 <RunRight/>
